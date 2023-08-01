b894dd778de15e1bd85831e0565b558f3224b818
Raul Ruiz <publiux@gmail.com>
2023-08-01 07:19:33 -0400

    Update LevelTest.php

--- tests/Models/LevelTest.php
+++ tests/Models/LevelTest.php
@@ -5,7 +5,7 @@ use LevelUp\Experience\Models\Level;
 
 uses()->group('levels');
 
-it(description: 'can create a level', closure: function (): void {
+it(description: 'can create a level without a title', closure: function (): void {
     $level = Level::add([
         'level' => 4,
         'next_level_experience' => 500,
@@ -16,6 +16,23 @@ it(description: 'can create a level', closure: function (): void {
     $this->assertDatabaseHas(table: 'levels', data: [
         'level' => 4,
         'next_level_experience' => 500,
+        'title' => null,
+    ]);
+});
+
+it(description: 'can create a level with a title', closure: function (): void {
+    $level = Level::add([
+        'level' => 4,
+        'next_level_experience' => 500,
+        'title' => 'Monkey Juggler',
+    ]);
+
+    expect(value: $level[0]->level)->toBe(expected: 4);
+
+    $this->assertDatabaseHas(table: 'levels', data: [
+        'level' => 4,
+        'next_level_experience' => 500,
+        'title' => 'Monkey Juggler',
     ]);
 });
 
