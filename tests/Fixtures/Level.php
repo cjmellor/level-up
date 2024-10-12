e5002029b4510bcc5a0515af19d9250759b87e35
Simone <simoneungaro@hotmail.it>
2024-10-12 14:14:23 +0200

    lint

--- tests/Fixtures/Level.php
+++ tests/Fixtures/Level.php
@@ -2,14 +2,6 @@
 
 namespace LevelUp\Experience\Tests\Fixtures;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use LevelUp\Experience\Concerns\GiveExperience;
-use LevelUp\Experience\Concerns\HasAchievements;
-use LevelUp\Experience\Concerns\HasStreaks;
-use LevelUp\Experience\Tests\Fixtures\Factories\UserFactory;
-
 class Level extends \LevelUp\Experience\Models\Level
 {
     public function extra_function(): string
--- tests/Models/LevelTestWithCustomModel.php
+++ tests/Models/LevelTestWithCustomModel.php
@@ -1,14 +1,12 @@
 <?php
 
 use LevelUp\Experience\Exceptions\LevelExistsException;
-use LevelUp\Experience\Tests\Fixtures\User;
 
 uses()->group('levels');
 
 beforeEach(function (): void {
     config()->set('level-up.models.level', \LevelUp\Experience\Tests\Fixtures\Level::class);
 });
-
 
 it(description: 'is a custom model', closure: function (): void {
     $levelClass = config('level-up.models.level');
